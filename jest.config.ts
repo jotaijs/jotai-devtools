6075253f49bbe265ad70d2f86450040e86db3c6d
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-01-06 21:46:11 -0500

    chore: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  rootDir: '.',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['<rootDir>/__tests__/**/*.(test).{ts,tsx}'],
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
+  moduleNameMapper: {
+    '^jotai-devtools$': '<rootDir>/src/index.ts',
+    '^jotai-devtools/(.*)$': '<rootDir>/src/$1.ts',
+  },
+  modulePathIgnorePatterns: ['dist'],
+  globals: {
+    __DEV__: true,
+  },
+  coverageReporters: ['json', 'html', 'text', 'text-summary'],
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}', '__tests__/**/*.{js,ts,tsx}'],
+  coverageDirectory: './coverage/',
+};
+
+export default config;
