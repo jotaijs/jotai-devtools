6c1572a5b22a8568fca3d9d21b0f0de05f12b1a5
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-02-04 19:38:03 -0500

    chore: upgrade storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,43 @@
+import { StorybookConfig } from '@storybook/react-webpack5';
+
+const webpack = require('webpack');
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __DEV__: configType === 'DEVELOPMENT',
+      }),
+    );
+
+    // Return the altered config
+    return config;
+  },
+};
+
+export default config;
