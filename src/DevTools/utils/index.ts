45c0a9c0f0bf25141b70b7b5a66668de82b7c390
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-05-07 12:41:56 -0400

    chore: remove unused deep-parsing feature

--- src/DevTools/utils/index.ts
+++ src/DevTools/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './get-type-of-atom-value';
-export * from './parse-atom-value';
 export * from './parse-debug-label';
 export * from './stringify-atom-value';
 export * from './create-memoized-emotion-cache';
