ca46ad2acf6610c757fae0fe2af3377f4fa3cfbf
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-06-11 22:32:13 -0400

    test: set TZ to UTC

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,7 @@
 import * as ResizeObserverModule from 'resize-observer-polyfill';
 
 (global as any).ResizeObserver = ResizeObserverModule.default;
+
+export default async () => {
+  process.env.TZ = 'UTC';
+};
