10c2ef832cda9a0bfffef77a91ee6ef7b65e40fd
Arjun <14841132+arjunvegda@users.noreply.github.com>
2023-06-12 17:01:13 -0400

    fix(create-timestamp.ts): add default locale

--- src/DevTools/utils/create-timestamp.ts
+++ src/DevTools/utils/create-timestamp.ts
@@ -1,5 +1,7 @@
+const getLocale = () => Intl.NumberFormat().resolvedOptions().locale || 'en-US';
+
 const formatTimeToTimeStamp = (time: number) =>
-  new Date(time).toLocaleString(undefined, {
+  new Date(time).toLocaleString(getLocale(), {
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric',
