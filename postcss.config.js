e46727dc560cc56392a2916b6cfe119201aa58b8
Elvin Chu <elvincth@gmail.com>
2024-07-19 02:31:46 +0800

    fix: only process @mantine libs

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
     'postcss-preset-mantine': {},
     'postcss-prefix-selector': {
       prefix: '#jotai-devtools-root',
+      includeFiles: [/node_modules\/@mantine/],
       transform(prefix, selector, prefixedSelector) {
         if (selector.startsWith('html') || selector.startsWith('body')) {
           return selector.replace(/^html|^body/, prefix);
