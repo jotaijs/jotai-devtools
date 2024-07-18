633f3e7fa2798c63a9877f34f8a5ab0331a5811c
Elvin Chu <elvincth@gmail.com>
2024-07-19 02:26:49 +0800

    fix: mantine css leak

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
     'postcss-preset-mantine': {},
+    'postcss-prefix-selector': {
+      prefix: '#jotai-devtools-root',
+      transform(prefix, selector, prefixedSelector) {
+        if (selector.startsWith('html') || selector.startsWith('body')) {
+          return selector.replace(/^html|^body/, prefix);
+        }
+        return prefixedSelector;
+      },
+    },
   },
 };
