5c19880dc52249b2243e51badd6a59e43e4bdd23
Arjun Vegda <14841132+arjunvegda@users.noreply.github.com>
2025-02-01 14:13:34 -0500

    fix: drop support for jotai store v1 (#173)
    * fix: drop support for jotai store v1
    
    * docs: update docs around prerequisites
    
    * fix: build error

--- src/DevTools/internal-jotai-store.ts
+++ src/DevTools/internal-jotai-store.ts
@@ -1,10 +1,6 @@
 import { createContext, useContext } from 'react';
-import { createStore } from 'jotai/vanilla';
-import { Store } from 'src/types';
+import type { Store } from 'src/types';
 
-// Don't use this directly in your components
-// use `useDevtoolsJotaiStoreOptions` instead
-export const internalJotaiStore = createStore();
 export const InternalDevToolsContext = createContext<Store | undefined>(
   undefined,
 );
