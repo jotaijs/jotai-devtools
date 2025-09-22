76924b3e157214c1ad95cdb539f9979b8816fd79
Arjun <14841132+arjunvegda@users.noreply.github.com>
2025-09-22 19:40:10 -0400

    chore: supress automatic tree-shaking warning during test run

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -3,6 +3,25 @@ import * as ResizeObserverModule from 'resize-observer-polyfill';
 
 (global as any).ResizeObserver = ResizeObserverModule.default;
 
+const originalWarn = console.warn;
+beforeAll(() => {
+  console.warn = (...args: any[]) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes(
+        '[jotai-devtools]: automatic tree-shaking in development mode is being deprecated',
+      )
+    ) {
+      return;
+    }
+    originalWarn(...args);
+  };
+});
+
+afterAll(() => {
+  console.warn = originalWarn;
+});
+
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: (query: string) => ({
